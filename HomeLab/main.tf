68818181cc2d4eb7516cf437f7419acff38cf556
Matthew Brian Cohen <cohen@matthewbriancohen.com>
2025-06-25 09:49:05 -0700

    added s3 backend

--- HomeLab/main.tf
+++ HomeLab/main.tf
@@ -5,6 +5,15 @@ terraform {
       version = "0.78.1"
     }
   }
+
+  backend "s3" {
+    bucket = "mbc-homelab-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-west-1"
+
+    dynamodb_table = "mbc-homelab-locks"
+    encrypt        = true
+  }
 }
 
 provider "proxmox" {
