3ed98705b2fee36f4311f70a55819e992f023cf3
Matthew Brian Cohen <cohen@matthewbriancohen.com>
2026-08-08 11:42:13 -0700

    turn off k8s test and change boot order

--- HomeLab/k8s.tf
+++ HomeLab/k8s.tf
@@ -28,6 +28,8 @@ resource "proxmox_virtual_environment_vm" "talos-control" {
     dedicated = 2048
   }
 
+  started     = false
+  boot_order    = ["scsi0","ide0","net0"]
   bios          = "ovmf"
   machine       = "q35"
   scsi_hardware = "virtio-scsi-pci"
@@ -101,6 +103,8 @@ resource "proxmox_virtual_environment_vm" "talos-worker" {
     dedicated = 2048
   }
 
+  started     = false
+  boot_order    = ["scsi0","ide0","net0"]
   bios          = "ovmf"
   machine       = "q35"
   scsi_hardware = "virtio-scsi-pci"
